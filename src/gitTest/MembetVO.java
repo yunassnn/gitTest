57935b7fd30b311783868c73495035af1490afe2
yuna <kasspo20@gmail.com>
2019-09-27 17:10:56 +0900

    MemberVo 클래스 추가...

--- src/gitTest/MembetVO.java
+++ src/gitTest/MembetVO.java
@@ -1,5 +1,30 @@
 package gitTest;
 
 public class MembetVO {
+	
+	private String bno;
+	private String name;
+	private String age;
+	
+	
+	public String getBno() {
+		return bno;
+	}
+	public void setBno(String bno) {
+		this.bno = bno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
 
 }
