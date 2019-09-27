208e60c30ea64d8b0ff1819239565e45f8f6a635
yuna <kasspo20@gmail.com>
2019-09-27 17:20:04 +0900

    studentVO toString 추가

--- src/gitTest/StudentVO.java
+++ src/gitTest/StudentVO.java
@@ -24,6 +24,13 @@ public class StudentVO {
 		this.dept = dept;
 	}
 	
+	@Override
+	public String toString() {
+		return "StudentVO [id=" + id + ", name=" + name + ", dept=" + dept + ", getId()=" + getId() + ", getName()="
+				+ getName() + ", getDept()=" + getDept() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
+				+ ", toString()=" + super.toString() + "]";
+	}
+	
 	
 
 }
