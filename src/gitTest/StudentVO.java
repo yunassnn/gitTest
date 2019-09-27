acc01db5c4e59211ff319dc64fd7729b845a0529
yuna <kasspo20@gmail.com>
2019-09-27 17:12:21 +0900

    Student 클래스 작성

---
+++ src/gitTest/StudentVO.java
@@ -0,0 +1,29 @@
+package gitTest;
+
+public class StudentVO {
+	
+	private int id;
+	private String name;
+	private String dept;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	
+	
+
+}
